39c1f0322a877d24da1faef1daae2411504a5871
stevenkcolin <steven.k.colin@gmail.com>
2020-05-23 16:31:56 +0800

    submit

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -3,9 +3,11 @@ pragma solidity >=0.4.21 <0.7.0;
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
+  int public thirdAngle;
 
   constructor() public {
     owner = msg.sender;
+    thirdAngle = 180;
   }
 
   modifier restricted() {
@@ -14,5 +16,13 @@ contract Migrations {
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
+  }
+
+  function setThirdAngle(int a, int b) public {
+    thirdAngle = 180 - a - b;
+  }
+
+  function getThirdAngle() constant public returns (int) {
+    return thirdAngle;
   }
 }
