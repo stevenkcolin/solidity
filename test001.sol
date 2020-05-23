85d0ee9002e6434cd340fa7a93061563b1358799
stevenkcolin <steven.k.colin@gmail.com>
2020-05-23 10:10:50 +0800

    Create test001.sol

---
+++ test001.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.22;
+
+contract ThirdAngle {
+  int third;
+  function otherAngle(int angle1, int angle2) {
+    // TODO your code here
+    int a1 = angle1;
+    int a2 = angle2;
+    third = 180 - a1 - a2;
+  }
+  function getThird() constant returns (int) {
+      return third;
+  }
+}
